6dad8f4ad2236341abd44952a0fd512ac8554f20
zino <theforgotten23623@gmail.com>
2023-01-13 22:25:05 +0800

    [exercises][bugfix] Fix a bug in modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -5,8 +5,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as fruit;
-    use self::veggies::CUCUMBER as veggie;
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
